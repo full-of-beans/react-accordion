cb6efaee53ef5cb1f517a60e2d598a622d4ee227
iamoperand <narora200@gmail.com>
2018-11-25 02:17:52 +0530

    [Feat]: BaseAccordion component done

---
+++ src/BaseAccordion.js
@@ -0,0 +1,73 @@
+import { Component } from 'react';
+import { func, arrayOf, shape } from 'prop-types';
+
+class BaseAccordion extends Component {
+  static defaultProps = {
+    stateReducer: (state, changes) => changes,
+    onStateChange: () => {},
+    openedIndexesThroughProps: undefined,
+    children: () => {}
+  };
+
+  static propTypes = {
+    stateReducer: func,
+    onStateChange: func,
+    openedIndexesThroughProps: arrayOf(shape({})),
+    children: func
+  };
+
+  state = {
+    openedIndexes: [0] // eslint-disable-line react/no-unused-state
+  };
+
+  getState = (state = this.state) => {
+    const { openedIndexesThroughProps } = this.props;
+
+    return {
+      openedIndexes:
+        openedIndexesThroughProps === undefined
+          ? state.openedIndexes
+          : openedIndexesThroughProps
+    };
+  };
+
+  internalSetState = (changes, callback = () => {}) => {
+    const { stateReducer, onStateChange } = this.props;
+    let allChanges;
+    this.setState(
+      state => {
+        const actualState = this.getState(state);
+        const changesObject =
+          typeof changes === 'function' ? changes(actualState) : changes;
+        allChanges = stateReducer(actualState, changesObject);
+        return allChanges;
+      },
+      () => {
+        onStateChange(allChanges);
+        callback();
+      }
+    );
+  };
+
+  handleItemClick = index => {
+    this.internalSetState(state => {
+      const closing = state.openedIndexes.includes(index);
+      return {
+        type: closing ? 'closing' : 'opening',
+        openIndexes: closing
+          ? state.openIndexes.filter(i => i !== index)
+          : [...state.openIndexes, index]
+      };
+    });
+  };
+
+  render() {
+    const { children } = this.props;
+    return children({
+      openIndexes: this.getState().openIndexes,
+      handleItemClick: this.handleItemClick
+    });
+  }
+}
+
+export default BaseAccordion;
